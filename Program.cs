2f6760a575fb6d6199f1fffcce2a91b23741def5
Vika <v.volnukhina2307@gmail.com>
2022-09-23 19:45:33 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+void SecondNumber ()
+{
+    Console.Write("Напишите любое трехзначное число: ");
+            int num = int.Parse(Console.ReadLine());
+            int x1 = num%100;
+            int result = x1/10;
+    Console.WriteLine(result);
+}
+
+SecondNumber ();
+
+
+
+
+
+
+
+
+
+
+
